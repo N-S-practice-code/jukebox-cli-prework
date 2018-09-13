fe2d0786136c2427709e8f49f5355b8f3a9207a8
Nico-ehs <nico.ehs05@gmail.com>
2018-09-13 19:33:29 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -58,7 +58,7 @@ def say_hello(name)
   "Hi #{name}!"
 end
  
-puts "Enter your name:"
-users_name = gets.chomp
+# puts "Enter your name:"
+# users_name = gets.chomp
  
-puts say_hello(users_name)
+# puts say_hello(users_name)
