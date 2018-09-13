7703e36d8fe940c6f9371d2f822efa4dffc7caeb
Nico-ehs <nico.ehs05@gmail.com>
2018-09-13 18:03:07 +0000

    automatically backed up by learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -11,6 +11,18 @@ songs = [
 ]
 
 def help
+  puts """I accept the following commands:
+- help : displays this help message
+- list : displays a list of songs you can play
+- play : lets you choose a song to play
+- exit : exits this program"""
+end
+
+def list(songs)
+  songs.each_with_index do |el, i|
+    
+  end
+end
 
 
 def say_hello(name)
